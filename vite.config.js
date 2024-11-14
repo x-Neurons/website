5d99522cd5d284fd3e1d7d4c9eabd9b07ae11bb1
Shahryar-Sohail <shahryar.ch123@gmail.com>
2024-11-14 23:14:51 +0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mkv'], // Include .mkv files as static assets
-  base: '/Company/',
+  base: '/website/',
 })
